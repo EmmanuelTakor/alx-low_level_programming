18574efa80c1f2f465ecb3bc8b9e7ff0f5fc006c
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-09 14:53:14 -0700

    commits

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'A calculations program'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
