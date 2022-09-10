acd4e381601522939c18b39791d1ac883e76889d
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-10 02:30:09 -0700

    commits

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,");
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ printf("with proper grammar, but the outcome is a piece of art,");
+return (0); }
