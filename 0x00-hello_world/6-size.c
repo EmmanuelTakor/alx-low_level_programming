bbc9740d56ab4209bb81dc2f754a49662f7f473d
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-11 15:49:31 -0700

    commits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,10 @@
  *
  * Return: Always 0 (Success)
  */
-printf("Size of a char: %c", sizeof(char));
-printf("Size of an int: %d", sizeof(int));
-printf("Size of a long int: %ld", sizeof(long int));
-printf("Size of a long long int: %ld", sizeof(long long int));
-printf("Size of a float: %f", sizeof(long int));
+int main(void)
+{printf("Size of a char: %c\n", sizeof(char));
+printf("Size of an int: %d\n", sizeof(int));
+printf("Size of a long int: %ld\n", sizeof(long int));
+printf("Size of a long long int: %ld\n", sizeof(long long int));
+printf("Size of a float: %f\n", sizeof(float));
+return (0);}
