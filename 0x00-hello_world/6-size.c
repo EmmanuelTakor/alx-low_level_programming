7011eb575677295e1d95279b63308c048104e31d
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-11 16:53:12 -0700

    commits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ char charType;
 long int longIntType;
 long long int longLongIntType;
 float floatType;
-printf("Size of a char: %c \n", sizeof(charType));
-printf("Size of an int: %d \n", sizeof(intType));
-printf("Size of a long int: %ld \n", sizeof(longIntType));
-printf("Size of a long long int: %ld \n", sizeof(longLongIntType));
-printf("Size of a float: %f \n", sizeof(floatType));
+printf("Size of a char: %zu \n", sizeof(charType));
+printf("Size of an int: %zu \n", sizeof(intType));
+printf("Size of a long int: %zu \n", sizeof(longIntType));
+printf("Size of a long long int: %zu \n", sizeof(longLongIntType));
+printf("Size of a float: %zu \n", sizeof(floatType));
 return (0); }
