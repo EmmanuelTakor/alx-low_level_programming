4c48c889eaff17975e0b72db951f6857b19fdc73
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-11 23:06:05 -0700

    commits

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,14 @@
-for (ch= ‘a’; ch<= ‘z’; ch++)
-
-                  putchar(ch);
-
-                  putchar (‘\n’) ;
-
-                  getch();
-
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{ putchar (letter); }
+putchar ('\n');
+return (0); }
