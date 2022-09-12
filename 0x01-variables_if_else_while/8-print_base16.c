80e22614e8e11c0d694e1780e1c5b4f8f9d49fc5
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-12 07:38:43 -0700

    commits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int base16;
+char letters;
+for (base16 = 0; base16 < 10; base16++)
+{ putchar(base16); }
+for (letters = 'a'; letters < 'e'; letters++)
+{putchar(letters); }
+putchar('\n');
+
+return (0); }
