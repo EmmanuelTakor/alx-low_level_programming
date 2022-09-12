0015403fe98ec818d4a8c2e4bf969a8c66777cf8
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-11 17:14:27 -0700

    commits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ char charType;
 long int longIntType;
 long long int longLongIntType;
 float floatType;
-printf("Size of a char: %zu byte(s)", sizeof(charType));
-printf("Size of an int: %zu byte(s)", sizeof(intType));
-printf("Size of a long int: %zu byte(s)", sizeof(longIntType));
-printf("Size of a long long int: %zu byte(s)", sizeof(longLongIntType));
-printf("Size of a float: %zu byte(s)", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0); }
