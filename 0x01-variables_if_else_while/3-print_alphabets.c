92cd51f908ca5553a310bb1eb377505b85aa6907
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-11 23:58:40 -0700

    commits

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{char upperCase;
-char lowerCase;
+{char lowerCase;
 for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
 { putchar(lowerCase);
 putchar(toupper(lowerCase)); }
