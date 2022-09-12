e1b2541aac261a478b930127d92fa6ccdd204c26
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-12 01:31:11 -0700

    commits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int num;
+for (num = 1; num < 10; num++)
+{printf("%d ,", num);
+return (0); } }
