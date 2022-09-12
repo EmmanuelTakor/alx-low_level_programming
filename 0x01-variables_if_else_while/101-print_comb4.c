33d7f787e05f55490e4778bbbae351b27a2b4aa7
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-12 09:29:34 -0700

    commits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ int num;
+for (num = 0; num <= 789; num++)
+{putchar (num + '0');
+if (num < 789
+{putchar(',');
+putchar(' '); }}
+return (0); }
