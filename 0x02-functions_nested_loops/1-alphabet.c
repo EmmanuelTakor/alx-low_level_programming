41a812de968b2482babd8e3f82587f96d0a8b57b
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-13 10:40:00 -0700

    commits

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,9 +7,21 @@
  *
  * Return: Always 0 (Success)
  */
+void print_alphabet(void);
+
 int main(void)
-{char letter;
-for (letter = 0; letter < 'z'; letter++; )
-{putchar(letter); }
-putchar("\n");
+{
+void print_alphabet(void);
+putchar('\n');
 return (0); }
+
+/**
+ * print_alphabet - Prints alphabets
+ * Description: "the program's description"
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{char letter;
+for (letter = 0; letter < 'z'; letter++)
+{putchar(letter); } }
