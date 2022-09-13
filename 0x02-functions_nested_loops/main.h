d6b7efb1772b0004e20e84bde15ab984bfcb44e3
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-13 12:17:41 -0700

    commits

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+/**
+ * print_alphabet - Prints alphabets
+ * Description: "the program's description"
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{char letter;
+for (letter = 0; letter < 'z'; letter++)
+{putchar(letter); } }
+
