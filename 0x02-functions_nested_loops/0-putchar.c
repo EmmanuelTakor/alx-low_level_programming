4d55281c5e13561d9d834c632d5d130415cea0cb
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-13 02:55:29 -0700

    commits

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char s[] = "_putchar\n";
+int i = 0;
+while (s[i] != '\n' && s[i] != '\0')
+{ putchar(s[i]);
+i++; }
+return (0); }
