96fbeedec0e6681275573d98b4d5f48e660f7d9a
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-14 15:25:58 -0700

    commits

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int)
+{if (n > 0)
+{n =-n;
+return (n);
+}
