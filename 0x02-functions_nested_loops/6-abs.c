ba1e79e716bd1e3085258d2c82485c043e8d0aee
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-14 15:38:40 -0700

    co0mmit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,10 @@
  * _abs - Entry point
  *
  * Description: 'the program's description'
- *
+ *@n:"n is a Parameter"
  * Return: Always 0 (Success)
  */
 int _abs(int n)
 {if (n < 0)
-n =-n;
+n =- n;
 return (n); }
