c2de6183f51f66157cd875e4740c583db8a1982d
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-14 14:28:15 -0700

    commits

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * Description: 'the program's description'
+ * @n:"n is a parameter"
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{int n;
+if (n > 0)
+{return (1);
+putchar('+'); }
+else if (n == 0)
+{return (0);
+putchar('0'); }
+else if (n < 0)
+{return ('-1');
+putchar('-'); }
+}
