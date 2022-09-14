3a54ec3f9d47e24dc0aacb7cd67b9db636cc7a22
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-14 11:10:23 -0700

    commits

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
-
 /**
- * void print_alphabet_x10(void) - check the code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - Prints alphabets
+ * Description: "the program's description"
+ * Return: Always 0 (Success)
  */
+
+
 void print_alphabet_x10(void)
 {char letter;
 int count;
 for (count = 0; count < 10; count++)
 {
 for (letter = 'a'; letter < 'z'; letter++)
-{ _putchar(letter); }}
-_putchar('\n'); }
+{ _putchar(letter); }
+_putchar('\n'); }}
