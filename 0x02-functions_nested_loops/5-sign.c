fe7a1175781e202f461f933f0b06e764dafabe99
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-14 15:07:17 -0700

    commits

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,13 +7,15 @@
  * Return: Always 0 (Success)
  */
 int print_sign(int n)
-{if (n > 0)
-{return (1);
-_putchar('+'); }
+{
+if (n > 0)
+{_putchar('+');
+return (1); }
 else if (n == 0)
-{return (0);
-_putchar('0'); }
+{_putchar('0');
+return (0); }
 else if (n < 0)
-{return (-1);
-_putchar('-'); }
+{_putchar('-');
+return (-1);
 }
+return (0); }
