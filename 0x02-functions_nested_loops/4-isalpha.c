1fbd3a3dfe4df79529158296d4d1981d6fa65f0a
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-14 13:31:52 -0700

    commits

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
  * int _isalpha - checks if c is aphabetic character
- *
  * Description: 'the program's description'
- *
  * Return: 1 0r 0 depending on input
  */
 int _isalpha(int c)
-{return c >= 'a' && c <= 'z'; }
+{return c >= 'a' && c <= 'z' && c>= 'A' && c >= 'B'; }
