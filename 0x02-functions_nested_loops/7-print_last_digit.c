b2d1d5edba220f2b85a877c01141abe244ff6f67
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-14 16:05:23 -0700

    commots

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - Entry point
+ * @n:"n is a parameter"
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int)
+{if (n < 0)
+n = -n;
+return (n % 10);
