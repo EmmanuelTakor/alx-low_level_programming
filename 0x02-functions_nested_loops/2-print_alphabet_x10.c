69071993f6583dc5b34cb948296a6c6bc4c6b42d
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-14 10:54:21 -0700

    commits

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0.
  */
-int void print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {char letter;
 int count;
 for (count = 0; count < 10; count++)
@@ -13,4 +13,4 @@ for (count = 0; count < 10; count++)
 for (letter = 'a'; letter < 'z'; letter++)
 { _putchar(letter); }}
 _putchar('\n');
-return (); }
+return (void); }
