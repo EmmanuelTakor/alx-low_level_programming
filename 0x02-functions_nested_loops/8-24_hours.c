380474b4e96c7e4dbf00dbde88992601870af93f
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-15 10:41:17 -0700

    commits

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,13 +7,13 @@
 
 void jack_bauer(void)
 {int i, j;
-for (i = 0; i < 24; 1++)
+for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
 {
 if (i < 10)
 { _putchar ('0');
-_putchar (i + '0') }
+_putchar (i + '0'); }
 else if (i >= 10)
 {_putchar((i / 10) + '0');
 _putchar(i % '0');
@@ -24,4 +24,4 @@ _putchar(j + '0'); }
 else if (j > 10)
 {_putchar(':');
 _putchar((j / 10) + '0');
-_putchar((j % 10) + '0'); } } }}
+_putchar((j % 10) + '0'); } } }}}
