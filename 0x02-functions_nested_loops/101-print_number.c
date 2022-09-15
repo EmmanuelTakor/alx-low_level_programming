5019e1162cd5da5ba33c52cd1f997e088e08ffd1
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-15 01:21:24 -0700

    commits

---
+++ 0x02-functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_number - prints interger
+ * @n:"parameter"
+ * Description: 'the program's description'
+ *
+* Return: Always 0 (Success)
+*/
+void print_number(int n)
+{int n;
+return (0); }
