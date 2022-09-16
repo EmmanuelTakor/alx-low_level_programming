3565ec26597a3685c8fcde783f191aceecb3a8dc
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-16 12:34:46 -0700

    commits

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,13 @@
 
 void print_most_numbers(void)
 {int i;
-for (i = 0; i >= 9; i++)
+for (i = 0; i <= 9; i++)
 {
 if (i != 2 && i != 4)
 {_putchar(i + '0'); }
 else
 {i--;
+
+
 i++; }}
 _putchar('\n'); }
