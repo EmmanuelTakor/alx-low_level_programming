0fb92f07a9bd7c7c71ace338f725eca7c958a9d2
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-15 18:01:33 -0700

    commits

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,6 @@
 #include "main.h"
 #include <time.h>
 #include <stdio.h>
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
-int i;
-i = 0;
-positive_or_negative(i);
-return (0); }
 
 /**
  * positive_or_negative - Entry point
@@ -30,4 +19,16 @@ else if (n == 0)
 {printf("%d is zero\n", n); }
 else
 {printf("%d is negative\n", n); }
+return (0); }
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
 return (0); }
