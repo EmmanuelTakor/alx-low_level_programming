7d3cc223226ce93b0d55d5c4144639a36aaef287
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-15 17:53:49 -0700

    commits

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <time.h>
+#include <stdio.h>
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -10,5 +11,23 @@ int main(void)
 int i;
 i = 0;
 positive_or_negative(i);
-return (0);
-}
+return (0); }
+
+/**
+ * positive_or_negative - Entry point
+ *
+ * Description: 'A calculations program'
+ * @i:"This is a parameter"
+ * Return: Always 0 (Success)
+ */
+int positive_or_negative(i)
+{int n;
+srand(time(0));
+n = i
+if (n > 0)
+{printf("%d is positive\n", n); }
+else if (n == 0)
+{printf("%d is zero\n", n); }
+else
+{printf("%d is negative\n", n); }
+return (0); }
