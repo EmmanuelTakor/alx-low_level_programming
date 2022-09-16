d8a0de39d349a2ab052dbaecab889a90bb9109ba
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-16 10:35:39 -0700

    commits

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _isupper - prints alphabets in lower case
  *@c:"c is a parameter"
@@ -11,6 +12,6 @@ int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 {return (1); }
-else
-return (0)
+else if (c >= 'a' && c <= 'z')
+return (0);
 }
