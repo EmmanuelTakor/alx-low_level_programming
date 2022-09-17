6a092a97e3ea694b496b5e690a2283dab777c164
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-16 21:02:16 -0700

    commits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * add - adds intergers
+ *   @int:"type of parameter to be returned"
+ *    * Return: Always 0.
+ */
+int add(int, int)
+{return int + int; }
