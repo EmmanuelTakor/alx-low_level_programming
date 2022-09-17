30782b4205e788bb8d70cfcfbeef91021af81d90
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-17 05:00:23 -0700

    commits

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,23 +8,6 @@
 void times_table(void)
 {int i;
 for (i = 0; i <= 9; i++)
-{_putchar((i * 0) + “0”);
+{_putchar((i * 0) + '0”);
 _putchar(", ");
-_putchar((i * 1) + “0”);
-_putchar(", ");
-_putchar((i * 2) + “0”);
-_putchar(", ");
-_putchar((i * 3) + “0”);
-_putchar(", ");
-_putchar((i * 4) + “0”);
-_putchar(", ");
-_putchar((i * 5) + “0”);
-_putchar(", ");
-_putchar((i * 6) + “0”);
-_putchar(", ");
-_putchar((i * 7) + “0”);
-_putchar(", ");
-_putchar((i * 8) + “0”);
-_putchar(", ");
-_putchar((i * 9) + “0”); }
-_putchar("\n"); }
+_putchar((i * 1) + “0”); }}
