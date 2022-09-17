3be826a11bf7fab9b79cc0e6ec4de19009806a53
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-17 03:53:48 -0700

    commits

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - creates the 9times table
+ * Return: Always 0.
+ */
+void times_table(void)
+{int i;
+for (i = 0; i <= 9; i++)
+{printf(i * 0);
+printf(", ");
+printf(i * 1);
+printf(", ");
+printf(i * 2);
+printf(", ");
+printf(i * 3);
+printf(", ");
+printf(i * 4);
+printf(", ");
+printf(i * 5);
+printf(", ");
+printf(i * 6);
+printf(", ");
+printf(i * 7);
+printf(", ");
+printf(i * 8);
+printf(", ");
+printf(i * 9);
+}}
