719e084f6e469a80a058c9611b967e603067404f
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-16 21:28:22 -0700

    commits

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  print_to_98 - prints natural numbers
+ *  @n:"n is parameter"
+ *    * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+for (n = n; n <= 98; n++)
+{printf("%d", n;
+printf(", ");
+}}
