ec9311df4cc055b4febb3cd745cdb9d23a300e4b
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-18 21:41:48 -0700

    commits

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
- /**
- * reset_to_98 - changes parameter passed to it
- * @*n:"*n is a parameter"
- * Description: 'the program's description'
- *
- * Return: Always 0 (Success)
- */
+/**
+* reset_to_98 - changes parameter passed to it
+* @n:"*n is a parameter"
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
-return (0);
 }
