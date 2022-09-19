d62b52f43d5f4c06c95b95c7baf70a1aa75a8257
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-18 22:25:09 -0700

    commits

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps parameters
+ * @a:"a p-arameter"
+ * @b:"a p-arameter"
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98; }
