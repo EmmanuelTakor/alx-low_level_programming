e76ef3b38c0564bce2cec1aee2b5e9e6b0d8edc3
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-20 16:14:54 -0700

    commits

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * _puts - function that prints string
- * Description: "the program's description"
- * @s: "This is an argument"
- * Return: Always 0 (Success)
- */
+* _puts - function that prints string
+* @str:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
 
 void _puts(char *str)
-{str[8] = "_putchar\n";
+{str = "hey";
 _putchar(str);
+_putchar('\n');
 }
