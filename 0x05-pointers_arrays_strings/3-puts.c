1581a7ffd819da89d93d327b9bf3ebd292e937b2
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-19 19:33:44 -0700

    commits

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function that prints string
+ * Description: "the program's description"
+ * @s: "This is an argument"
+ * Return: Always 0 (Success)
+ */
+
+void complete(char *s)
+{s[88888888] = "_putchar\n";
+_putchar(s);
+}
