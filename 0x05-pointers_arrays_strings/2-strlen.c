d9c4c08632972051b95843cf2ac5a73a0be9d783
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-19 19:18:17 -0700

    commits

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* main - Entry point
+* _strlen - returns lengths of strings
 * Description: "the program's description"
 * @s: "This is an argument"
 * Return: Always 0 (Success)
 */
 int _strlen(char *s)
-{int i;
-for (i = 0; s[i] != '\0';
-++i);
+{
+int i;
+for (i = 0;
+s[i] != '\0';
+++i)
 printf("Length of the string: %d", i);
 return (0);
 }
