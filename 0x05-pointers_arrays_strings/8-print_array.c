1a0eb276da133dc0977fc4166d89a3f8d1516694
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-20 19:37:36 -0700

    commits

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* print_array - function that prints string
+* @a:"This is a parameter"
+* @n:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+void print_array(int *a, int n)
+{int i;
+for (i = 0; i <= n - 1; i++)
+{putchar(s[i]); }
+putchar('\n'); }
