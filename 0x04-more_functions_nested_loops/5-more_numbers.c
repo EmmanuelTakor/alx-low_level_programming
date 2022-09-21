0f55c6b6376cdf3d582312e56aed3f6eeb600b71
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-20 20:45:12 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,15 @@
-
 #include "main.h"
 /**
  * more_numbers - prints numbers 10times
  * Description: 'the program's description'
  *
-* Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 void more_numbers(void)
 {int count;
 int i;
-for (count = 0; count <= 10; count++)
+for (count = 0; count < 10; count++)
 {
 for (i = 0; i <= 14 ; i++)
-{_putchar(i + '0'); }
-_putchar('\n'); }}
+{putchar(i); }
+putchar('\n'); }}
