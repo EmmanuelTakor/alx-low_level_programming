eb8844cf7c233a6d4509f4ace1654d920d34e3a3
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-20 18:25:52 -0700

    commits

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,17 @@
 */
 
 void rev_string(char *s)
-{int i, n;
-n = strlen(s);
-for (i = n - 1; i >= 0; i--)
-{putchar(s[i]); } }
+{char tmp;
+int i, length1, length2;
+
+length1 = 0;
+length2 = 0;
+while (s[length1] != '\0')
+length1++;
+length2 = length1 - 1;
+for (i = 0; i < length1 / 2; i++)
+{tmp = s[i];
+s[i] = s[length2];
+s[length2] = tmp;
+length2 -= 1;
+} }
