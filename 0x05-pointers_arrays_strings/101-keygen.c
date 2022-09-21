7e680fb945ab2fae064ef50ef69a6a9876001a1d
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-20 19:52:41 -0700

    commits

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+/**
+* password - function that prints string
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+int password(void)
+{return (rand()); }
