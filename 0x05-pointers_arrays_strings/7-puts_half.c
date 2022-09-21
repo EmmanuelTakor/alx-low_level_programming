217a6ba428a4cdd558fe088e4244a537da3371f3
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-20 19:06:57 -0700

    commits

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 /**
-* print_rev - function that prints half of string
-* @s:"This is a parameter"
+* puts_half - function that prints half of string
+* @str:"This is a parameter"
 * Description: "the program's description"
 * Return: Always 0 (Success)
 */
 
-void print_rev(char *s)
+void puts_half(char *str)
 {int i, n;
 n = strlen(s) / 2;
 for (i = n - 1; i < 0; i++)
