e70f5b822b479826a4ccd952d9689019c086b6d0
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-22 00:09:41 -0700

    commits

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* cap_string - function that prints string
+* @x:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+char *cap_string(char *x)
+{char spc[] = '32,9,'\n', ', ', '; ' ,
+'. ' ,' !' ,' ? ', '"','( ' , ') ' ,'{ ' ,'} ' };
+int len = 13;
+int a = 0, i;
+while (x[a])
+{ i = 0;
+while (i < len)
+{if ((a == 0 || s[a-1] ==spc[i])
+&& (s[a] >= 97 && s[a] <= 122))
+s[a] = s[a] - 32;
+i++; }
+a++; }
+return (x); }
