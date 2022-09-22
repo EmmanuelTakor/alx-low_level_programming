5b93c9a10df3e8d96ee29781d6dcc146419309d9
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-21 20:57:30 -0700

    commits

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strcmp - function that prints string
+* @s1:"This is a parameter"
+* @s2:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+int _strcmp(char *s1, char *s2)
+{int result;
+result = strcmp(s1, s2);
+return (result); }
