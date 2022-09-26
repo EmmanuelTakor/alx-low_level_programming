25a735d034d667738411a48ad6e8418733bea2e3
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-26 14:09:25 -0700

    commits

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _memset - fills memory with a constant byte
+* @s:"This is a parameter"
+* @n:"This is a parameter"
+* @b:"This is a parameter"
+* Description: "Tjis function fills memory"
+* Return: Always 0 (Success)
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(s + i) = b;
+}
+return (s);
+}
