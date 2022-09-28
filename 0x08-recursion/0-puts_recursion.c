9a41702ca99078f77d0c0b74d019416654509c03
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-28 00:45:33 -0700

    commits

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
-* void _puts_recursion(char *s);
+* _puts_recursion - a recursive function
 * @s:"This is a parameter"
 * Description: "the program's description"
 * Return: Always 0 (Success)
 */
 
 void _puts_recursion(char *s)
-{if (*s == '\0')
+{
+if (*s == '\0')
 {_putchar('\n');
 return; }
 _putchar (*s);
