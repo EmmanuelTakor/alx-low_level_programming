d8456673cf2cff81ee5f3bf0e507c0a2b5385e8a
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-28 00:58:38 -0700

    commits

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _print_rev_recursion - prints string in reverse
+* @s:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+_print_rev_recursion(s + 1);
+putchar(*s); }}
