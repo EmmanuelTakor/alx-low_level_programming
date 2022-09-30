368b65c23ee056c9964079bd5d2b75fc20012768
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-30 13:49:46 -0700

    commits

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - writes character vc to stdout
+* @argc:"This is a parameter"
+* @argv:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+int a, b;
+if (argc == 3)
+{ a = atoi(argv[i]);
+b = atoi(argv[2]);
+printf("%d\n", a, *b);
+return (0); }
+printf("Error\n");
+return (0); }
