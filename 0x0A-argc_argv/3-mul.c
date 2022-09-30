4c1357a51ef66d10a6c8bb15c8748f2ed90ad058
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-30 13:52:40 -0700

    commits

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,9 +13,9 @@ int main(int argc, char *argv[])
 {
 int a, b;
 if (argc == 3)
-{ a = atoi(argv[i]);
+{ a = atoi(argv[1]);
 b = atoi(argv[2]);
-printf("%d\n", a, *b);
+printf("%d\n", a *b);
 return (0); }
 printf("Error\n");
 return (0); }
