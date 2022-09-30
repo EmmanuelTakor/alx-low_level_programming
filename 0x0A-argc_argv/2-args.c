485c0db5b485a30ae6e52e729c80ad696f711b12
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-30 13:33:40 -0700

    co mmits

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - writes character vc to stdout
+* @argc:"This is a parameter"
+* @argv:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{int count = 0;
+if (argc > 0)
+{
+while (count < argc)
+{ printf("%s\n", argv[count]);
+count++ }}
+return (0); }
