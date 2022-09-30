295ed78109fb2f733c8f4e448ce900a029122098
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-09-30 11:47:46 -0700

    commits

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,10 @@
+include "main.h"
+/**
+* _putchar - writes character vc to stdout
+* @c:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+int _putchar(char c)
+{ return (write(1, &c, 1)); }
