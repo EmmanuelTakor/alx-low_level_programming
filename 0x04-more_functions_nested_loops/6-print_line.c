70a47b721946c2f492ba55054551cf88a828f2ed
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-10-03 01:28:54 -0700

    commits

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,13 +5,13 @@
  * @n:"number of times line should be printed"
  * Description: 'the program's description'
  *
-* Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 void print_line(int n)
 {
 int i;
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
 if (n <= 0)
 { putchar('\n'); }
