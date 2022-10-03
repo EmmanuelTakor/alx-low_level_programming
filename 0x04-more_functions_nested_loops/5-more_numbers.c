b84596cd209fc541a57f5374260419501f901137
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-10-02 21:23:20 -0700

    commits

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,5 +11,8 @@ int i;
 for (count = 0; count < 10; count++)
 {
 for (i = 0; i <= 14 ; i++)
-{_putchar((i) + '0'); }
-_putchar('\n'); }}
+{
+if (i >= 10)
+_putchar((i / 10) + '0'); }
+_putchar((i % 10) + '0'); }
+_putchar('\n'); }
