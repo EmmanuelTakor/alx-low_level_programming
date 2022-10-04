a297f3cd7123b942ec91dbdbe1e3a657c89120a1
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-10-04 08:21:08 -0700

    commits

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,16 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 int putchar(int achar);
-char *create_array(unsigned int size, char c)
-{
-
-char *array;
-unsigned int index;
-if (size == 0)
-return (NULL);
-array = malloc(sizeof(char) * size);
-if (array == NULL)
-return (NULL);
-for (index = 0; index < size; index++)
-array[index] = c;
-return (array);
+char *create_array(unsigned int size, char c);
