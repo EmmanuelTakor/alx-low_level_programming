d3930ea903511c4c2aad99ca220861197e3d87b4
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-10-04 08:06:50 -0700

    commits

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,8 +9,13 @@
 */
 
 char *create_array(unsigned int size, char c)
-{ c[5] = {"S", "T", "A", "N", "D"};
-if (sizeof(c) != 0)
-{return NULL; }
-else
-*size = &c; }
+{char *array;
+unsigned int index;
+if (size == 0)
+return (NULL);
+array = malloc(sizeof(char) * size);
+if (array == NULL)
+return (NULL);
+for (index = 0; index < size; index++)
+array[index] = c;
+return (array); }
