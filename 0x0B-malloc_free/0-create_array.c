7e56c1e1fe9b12bf3bce15dc50340c9ed7a68a83
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-10-04 07:54:46 -0700

    commits

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* create_array - writes character vc to stdout
+* @size:"This is a parameter"
+* @c:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+char *create_array(unsigned int size, char c)
+{ c[5] = {"S", "T", "A", "N", "D"};
+if (sizeof(c) != 0)
+{return NULL; }
+else
+*size = &c; }
