27128e98fa7be115be2ffd032717478f3dc1d204
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-10-04 08:52:46 -0700

    commits

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strdup - writes character vc to stdout
+* @str:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+char *_strdup(char *str)
+{ char *cpy;
+int index, len;
+if (str == NULL)
+return (NULL);
+for (index = 0; str[index]; index++)
+len++;
+cpy = malloc(sizeof(char) * (len + 1));
+if (cpy == NULL)
+return (NULL);
+for (index = 0; str[index]; index++)
+{ cpy[index] = str[index]; }
+cpy[len] = '\0';
+return (cpy); }
+
