1a1361e91c12aeae5fb8d85f40d860ddd7bcebec
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-10-05 23:24:31 -0700

    commits

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n);
 Prototype: char *_memcpy(char *dest, char *src, unsigned int n);
 Prototype: char *_strchr(char *s, char c);
 Prototype: unsigned int _strspn(char *s, char *accept);
