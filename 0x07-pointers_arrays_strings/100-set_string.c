440fea5770f52b466258ddb16511e8bb9ea15f12
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-10-12 05:55:15 -0700

    commits

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* set_string - string
+* @s:"This is a parameter"
+* @to:"This is a parameter"
+* Description: "the program's description"
+* Return: Always 0 (Success)
+*/
+
+void set_string(char **s, char *to)
+{*s = to;
+}
