c3c3900270b36b0d4819842b4e218c5fc128e1ec
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-11-14 20:42:13 -0800

    commits

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,9 +5,10 @@
  * struct dog - Struct dog
  * @name: name
  * @age: age
- * @owner: owner
+ * owner: owner
  * Description: This struct is for dog
  */
+
 struct dog
 {
 char *name;
@@ -19,4 +20,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif /*DOG_H*/
