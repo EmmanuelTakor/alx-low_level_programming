0e2c4fed0bfca07eaad52af6dce356df34a48071
EmmanuelTakor <emmanueltakor4@gmail.com>
2022-11-24 07:02:13 -0800

    commits

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void _attribute_((constructor)) hare(void);
+/**
+ * hare - Prints a string before
+ *
+ */
+void hare(void)
+{
+printf("You're beat! and yet, you must allow,"\n
+I bore my house upon my back !\n");
+}
